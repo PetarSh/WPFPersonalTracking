7eeed9db70b65c3ad98aedde702e2a9fb3d76802
Petar Shatanovski <petar.shatanovski@quipu.de>
2022-05-11 15:38:35 +0200

    Employee List

---
+++ WPFPersonalTracking/Views/EmployeeList.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFPersonalTracking.DB;
+
+namespace WPFPersonalTracking.Views
+{
+    /// <summary>
+    /// Interaction logic for EmployeeList.xaml
+    /// </summary>
+    public partial class EmployeeList : UserControl
+    {
+        public EmployeeList()
+        {
+            InitializeComponent();
+        }
+        PERSONALTRACKINGContext db = new PERSONALTRACKINGContext();
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmbDepartment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
