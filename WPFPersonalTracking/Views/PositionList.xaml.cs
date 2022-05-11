824d940921c4777d9cbb9be94c9408080b0bb31f
Petar Shatanovski <petar.shatanovski@quipu.de>
2022-05-11 14:47:18 +0200

    Position Page Update Add Delete

--- WPFPersonalTracking/Views/PositionList.xaml.cs
+++ WPFPersonalTracking/Views/PositionList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPFPersonalTracking.DB;
+using WPFPersonalTracking.ViewModels;
 
 namespace WPFPersonalTracking.Views
 {
@@ -27,24 +29,65 @@ namespace WPFPersonalTracking.Views
         }
 
         PERSONALTRACKINGContext db = new PERSONALTRACKINGContext();
+        void FillGrid()
+        {
+            var list = db.Positions.Include(x => x.Department).Select(a => new {
+                positionId = a.Id,
+                PositionName = a.PositionName,
+                departmentId = a.DepartmentId,
+                departmentName = a.Department.DepartmentName
+            }).OrderBy(x => x.PositionName).ToList();
+            List<PositionModel> modellist = new List<PositionModel>();
+            foreach (var item in list)
+            {
+                PositionModel model = new PositionModel();
+                model.Id = item.positionId;
+                model.PositionName = item.PositionName;
+                model.DepartmentName = item.departmentName;
+                model.DepartmentId = item.departmentId;
+                modellist.Add(model);
+            }
+            GridPosition.ItemsSource = modellist;
+        }
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            PositionPage page = new PositionPage();
+            page.ShowDialog();
+            FillGrid();
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
-
+            PositionModel model = (PositionModel)GridPosition.SelectedItem;
+            if (model != null && model.Id != 0)
+            {
+                PositionPage page = new PositionPage();
+                page.model = model;
+                page.ShowDialog();
+                FillGrid();
+            }
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            PositionModel model = (PositionModel)GridPosition.SelectedItem;
+            if (model != null && model.Id != 0)
+            {
+                if (MessageBox.Show("Are you sure to delete", "Question", MessageBoxButton.YesNo
+                    , MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    Position position = db.Positions.Find(model.Id);
+                    db.Positions.Remove(position);
+                    db.SaveChanges();
+                    MessageBox.Show("Position was deleted");
+                    FillGrid();
+                }
+            }
         }
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-
+            FillGrid();
         }
     }
 }
